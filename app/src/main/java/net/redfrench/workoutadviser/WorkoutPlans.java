5e421a8fbfa941d5ee4cd1680a4f709c41ecd0c1
Red-French <red@redfrench.net>
2017-03-18 16:36:46 -0500

    add getWorkout() which pushes data into array used to populate textView in FindWorkoutActivity

--- app/src/main/java/net/redfrench/workoutadviser/WorkoutPlans.java
+++ app/src/main/java/net/redfrench/workoutadviser/WorkoutPlans.java
@@ -1,8 +1,29 @@
 package net.redfrench.workoutadviser;
 
-/**
- * Created by user on 3/18/17.
- */
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkoutPlans {
+
+    List<String> getWorkout (String workoutType) {
+
+        List<String> workout = new ArrayList<>();
+
+        if (workoutType.equals("Chest")) {
+            workout.add("Bench Press");
+            workout.add("Cable Fly");
+        }
+        else if (workoutType.equals("Triceps")) {
+            workout.add("Triceps Extensions");
+            workout.add("Triceps Pushdowns");
+        }
+        else if (workoutType.equals("Shoulders")) {
+            workout.add("Shoulder Press");
+        }
+        else if (workoutType.equals("Biceps")) {
+            workout.add("Biceps Curls");
+        }
+
+        return workout;
+    }
 }
